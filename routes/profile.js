ffd414d203d236398d3216cc67fa3ea8eef9dc16
Oleh Protsenko <designofhumanity@gmail.com>
2023-06-25 13:02:04 +0300

    ai improvements

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 const { authenticateUser } = require('../middleware/auth');
 const { getUserProfile, updateUserProfile } = require('../controllers/profile');
 const { validationResult } = require('express-validator');
@@ -8,6 +9,13 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const Joi = require('joi');
 const moment = require('moment');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+const winston = require('winston');
+
+// Use body-parser middleware to parse the request body
+router.use(bodyParser.json());
 
 // Route for handling GET requests to the user's profile
 router.get('/', authenticateUser, async (req, res) => {
@@ -16,7 +24,7 @@ router.get('/', authenticateUser, async (req, res) => {
     const userProfile = await getUserProfile(userId);
     res.json(userProfile);
   } catch (error) {
-    console.error(error);
+    winston.error(error);
     res.status(500).send('Internal server error');
   }
 });
@@ -30,7 +38,7 @@ router.put('/', authenticateUser, upload.single('profilePicture'), async (req, r
     }
 
     const userId = req.user.id;
-    const { name, bio, dateOfBirth } = req.body;
+    const { name, bio, dateOfBirth, password } = req.body;
 
     // Validate the date of birth using Joi
     const schema = Joi.object({
@@ -42,11 +50,16 @@ router.put('/', authenticateUser, upload.single('profilePicture'), async (req, r
       return res.status(400).json({ msg: 'Invalid date of birth' });
     }
 
+    // Hash and salt the password using bcrypt
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     // Update the user's profile
     const user = await User.findById(userId);
     user.name = name;
     user.bio = bio;
     user.dateOfBirth = dateOfBirth;
+    user.password = hashedPassword;
 
     if (req.file) {
       user.profilePicture = req.file.filename;
@@ -54,11 +67,20 @@ router.put('/', authenticateUser, upload.single('profilePicture'), async (req, r
 
     await user.save();
 
+    // Generate a JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    // Set the JWT token as a cookie
+    res.cookie('token', token, { httpOnly: true });
+
     res.json(user);
   } catch (error) {
-    console.error(error);
+    winston.error(error);
     res.status(500).send('Internal server error');
   }
 });
+
+// Use helmet middleware to add security headers
+router.use(helmet());
 
 module.exports = router;
